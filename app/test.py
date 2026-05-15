41641769b7871104afd6cfb3657762a2f4d72b51
Finnley <finnleybaraka89@gmail.com>
2026-05-15 17:59:56 +0300

    Add a home page, an about page, contact page and privacy policy

--- app/test.py
+++ app/test.py
@@ -13,6 +13,26 @@ def home():
     return render_template("home.html")
 
 
+@test_bp.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@test_bp.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+
+@test_bp.route("/terms")
+def terms():
+    return render_template("terms.html")
+
+
+@test_bp.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 @test_bp.route("/googlefdab6438de2f962c.html")
 def google_site_verification():
     project_root = Path(current_app.root_path).parent
